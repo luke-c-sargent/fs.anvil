c20607aff7d9a762fb5a103ebce5d4453f5cdb05
Luke Sargent <luke.c.sargent@gmail.com>
2020-08-25 11:19:55 -0700

    branch switch checkin

--- anvilfs/reference.py
+++ anvilfs/reference.py
@@ -1,5 +1,53 @@
+from io import BytesIO
+
 import firecloud.api as fapi
+from google.cloud import storage
 
 from .base import BaseAnVILFolder, BaseAnVILFile
 
-class ReferenceData(BaseAnV)
+class ReferenceDataFile(BaseAnVILFile):
+    def __init__(self, name, url, size, last_modified=None):
+        self.name = name
+        self.url = url
+        self.last_modified
+
+    @classmethod
+    def make_rdfs(cls, objs):
+        result = []
+        if isinstance(objs, str):
+            objs = [objs]
+        elif isinstance(objs, dict):
+            objs = objs["items"]
+        for o in objs:
+            name = o.split("/")[-1]
+            result.append(cls(name, o))
+        return result
+
+    def get_bytes_handler(self):
+        return resolve_url()
+
+    def resolve_url(self):
+        # determine type
+        scheme = self.url.split("://")[0]
+        if scheme == "gs":
+            self._resolve_google(self.url)
+        elif scheme == "drs":
+            self._resolve_drs(self.url)
+        elif scheme == "http" or scheme == "https":
+            self._resolve_http(self.url)
+        else:
+            raise Exception("{}: schema '{}' not supported".format(self.__class__.__name__, schema))
+
+    def _resolve_google(self, url):
+        buffer = BytesIO()
+        c = storage.Client()
+        c.download_blob_to_file(url, buffer)
+        return buffer
+
+    def _resolve_drs(self, url):
+        pass
+        # return buffer
+
+    def _resolve_http(self, url):
+        pass
+        # return buffer
--- anvilfs/workspace.py
+++ anvilfs/workspace.py
@@ -32,15 +32,31 @@ class Workspace(BaseAnVILFolder):
     def __init__(self, namespace_reference,  workspace_name):
         self.namespace = namespace_reference
         resp = self.fetch_api_info(workspace_name)
+        attributes = resp["workspace"]["attributes"]
         try:
             super().__init__(workspace_name, resp["workspace"]["lastModified"])
         except KeyError as e:
             print("Error: Workspace fetch_api_info({}) fetch failed".format(workspace_name))
-        baf = BaseAnVILFolder("Other Data/")
-        self[baf] = None
+        # bucket folder
+        bucket_baf = BaseAnVILFolder("Other Data/")
+        self[bucket_baf] = None
+        # ref data folder
+        ref_baf = BaseAnVILFolder("Reference Data/")
+        self[ref_baf] = None
+        refs = self.ref_extractor(attribs)
+        for source in refs:
+            # source, e.g. hg38
+            source_baf = BaseAnVILFolder(source+"/")
+            ref_baf[source_baf] = None
+            # reftype, e.g. axiomPoly_resource_vcf
+            for reftype in refs[source]:
+                reftype_baf = BaseAnVILFolder(reftype+"/")
+                source_baf[reftype_baf] = None
+                contents = ReferenceDataFile.make_rdfs(refs[source][reftype])
+                for c in contents:
+                    reftype_baf[c] = None
 
-        # populate workspacedata
-        attributes = resp["workspace"]["attributes"]
+        # populate workspace data
         workspacedata = dict(attributes)
         blocklist_prefixes = [
             "referenceData_",
@@ -51,9 +67,26 @@ class Workspace(BaseAnVILFolder):
                 if datum.startswith(blocked):
                     del workspacedata[datum]
         if workspacedata:
-            baf[WorkspaceData("WorkspaceData.tsv", workspacedata)] = None
-        baf[WorkspaceBucket(resp["workspace"]["bucketName"])] = None
+            bucket_baf[WorkspaceData("WorkspaceData.tsv", workspacedata)] = None
+        bucket_baf[WorkspaceBucket(resp["workspace"]["bucketName"])] = None
         self.bucket_name = resp["workspace"]["bucketName"]
+        # populate reference data
+
+    def ref_extractor(self, attribs):
+        # structure:
+        # { "source": {
+        #      "reftype": <files, str or list> }}
+        result = {}
+        for ref in [r for r in attribs if r.startswith("referenceData_")]:
+            chunked = ref.split("_")
+            source = chunked[1]
+            if source not in result:
+                result[source] = {}
+            reftype = "_".join(chunked[2:])
+            result[source].extend({
+                reftype: attribs[ref]
+            })
+        return result
 
     def fetch_api_info(self, workspace_name):
         fields = "workspace.attributes,workspace.bucketName,workspace.lastModified"
